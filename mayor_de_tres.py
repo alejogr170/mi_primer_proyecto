d6656dd2a8fb28304108e25f3991452a63922e09
alejogr170 <alejogr170@gmail.com>
2019-06-09 01:06:36 +0200

    Sca el número mayor

---
+++ mayor_de_tres.py
@@ -0,0 +1,10 @@
+
+
+primer_numero = int(input("Dime un numero: "))
+segundo_numero = int(input("Dime otro numero: "))
+tercer_numero = int(input("Dime el ultimo numero: "))
+
+numero_mayor = max(primer_numero, segundo_numero, tercer_numero)
+
+print("El número más grande es el {}".format(numero_mayor))
+

522d1b4f4d67ad41021f303c0365882139654357
alejogr170 <alejogr170@gmail.com>
2019-06-09 20:20:35 +0200

    Forma compleja

--- tomar_helado.py
+++ tomar_helado.py
@@ -1,11 +1,16 @@
 
 
-te_apetece_un_helado= input("Te apetece un helado? (Si/No)").upper()
-tienes_dinero = input("Tienes dinero? (Si/No)").upper()
-alguien_te_invita= input("Tienes a alguien que te invite? (Si/No)").upper()
-esta_el_carrito = input("Esta el carrito de los helados? (Si/No)").upper()
+te_apetece_un_helado_input = input("Te apetece un helado? (Si/No)").upper()
+tienes_dinero_input = input("Tienes dinero? (Si/No)").upper()
+alguien_te_invita_input = input("Tienes a alguien que te invite? (Si/No)").upper()
+esta_el_carrito_input = input("Esta el carrito de los helados? (Si/No)").upper()
 
-if te_apetece_un_helado == "SI" and (tienes_dinero== "SI" or alguien_te_invita == "SI") and esta_el_carrito == "SI":
-    print("Entonces.... ¡Que aproveche!")
+te_apetece_un_helado = te_apetece_un_helado_input == "SI"
+tienes_dinero = tienes_dinero_input == "SI"
+alguien_te_invita = alguien_te_invita_input == "SI"
+esta_el_carrito = esta_el_carrito_input == "SI"
+
+if te_apetece_un_helado and (tienes_dinero or alguien_te_invita) and esta_el_carrito:
+    print("Entonces... Ya estas tardando!")
 else:
-    print("Otro dia sera!")
+    print("Tranquilo, otra vez sera :[")

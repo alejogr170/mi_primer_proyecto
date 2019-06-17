4934a16a57a66e3e8a4522603a6fbdccec84f4ac
alejogr170 <alejogr170@gmail.com>
2019-06-17 17:24:59 +0200

    numeros_pares_e_impares

--- probando_while.py
+++ probando_while.py
@@ -3,6 +3,8 @@ inicio_cuenta = 100
 
 while inicio_cuenta>0:
     print (inicio_cuenta)
+    if inicio_cuenta % 2 == 0:
+        print ("Es par")
+    else:
+        print("Es impar")
     inicio_cuenta -= 1
-
-print ("Boom")

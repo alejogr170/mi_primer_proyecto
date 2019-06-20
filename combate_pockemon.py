8e5e240ac8de6f4d5a0b75d1b787f2282fed51ad
alejogr170 <alejogr170@gmail.com>
2019-06-20 16:47:03 +0200

    terminado_a_mejorar

--- combate_pockemon.py
+++ combate_pockemon.py
@@ -3,9 +3,6 @@ pockemon_elegido = input("Contra que pockemon quieres combatir? ( Squirtle / Cha
 
 vida_pikachu = 100
 vida_enemigo = 0
-ataque_bulbasaur = 10
-ataque_squirtle = 8
-ataque_charmander = 7
 
 if pockemon_elegido == "SQUIRTLE":
     vida_enemigo = 90
@@ -16,12 +13,27 @@ if pockemon_elegido == "CHARMANDER":
 if pockemon_elegido == "BULBASAUR":
     vida_enemigo = 100
 
-while vida_pikachu > 0 and vida_enemigo > 0 :
+while vida_pikachu > 0 and vida_enemigo > 0:
     ataque_elegido = input("¿Que ataque deseas usar ( Bola voltio / Chispazo)?:").upper()
     if ataque_elegido == "BOLA VOLTIO":
         vida_enemigo -= 12
     if ataque_elegido == "CHISPAZO":
         vida_enemigo -= 10
-    print("La vida de tu oponente es:  " + vida_enemigo)
-
-
+    print("La vida de tu oponente es {}".format(vida_enemigo))
+    if pockemon_elegido == "SQUIRTLE":
+        vida_pikachu -= 8
+        print("Squirtle te hace un ataque de 8 de daño")
+        print("La vida de pikachu es de {}".format(vida_pikachu))
+    if pockemon_elegido == "CHARMANDER":
+        vida_pikachu -= 7
+        print("Charmander te hace un ataque de 7 de daño")
+        print("La vida de pikachu es de {}".format(vida_pikachu))
+    if pockemon_elegido == "BULBASAUR":
+        vida_pikachu -= 10
+        print("Bulbasaur te hace un ataque de 10 de daño")
+        print("La vida de pikachu es de {}".format(vida_pikachu))
+if vida_enemigo <= 0:
+    print("{} esta fuera de combate".format(pockemon_elegido))
+if vida_pikachu <= 0:
+    print("PIKACHU esta fuera de combate")
+print("Final del combate")

eefe8d82d826dd016d71735b9b3f66d7d0906442
alejogr170 <alejogr170@gmail.com>
2019-06-26 23:43:32 +0200

    utilizando .append

--- lista_de_la_compra.py
+++ lista_de_la_compra.py
@@ -2,7 +2,13 @@ print()
 print()
 
 print('Está es la lista de la compra:')
-lista_de_la_compra = ['Lechugas', 'Aceitunas', 'Pimientos', 'Tomates', 'Azucar']
+
+lista_de_la_compra = []
+input_usuario = ""
+
+while input_usuario != "FIN":
+    input_usuario = input("Escribe lo que necesites (para salir escribe FIN) :")
+    lista_de_la_compra.append(input_usuario)
 
 cantidad_de_elementos = len(lista_de_la_compra)
 indice = 0

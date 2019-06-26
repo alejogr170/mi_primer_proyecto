fd15ebab4b317e40e07ff63d63c681da22443775
alejogr170 <alejogr170@gmail.com>
2019-06-26 22:07:44 +0200

    fahrenheit_converter

---
+++ fahrenheit_to_celsius.py
@@ -0,0 +1,8 @@
+print()
+print()
+
+fahrenheit = float(input('What is the temperature?'))
+
+celsius = float((fahrenheit - 32)/1.8)
+
+print('The temperature in Celsius degrees is {}º'.format(celsius))

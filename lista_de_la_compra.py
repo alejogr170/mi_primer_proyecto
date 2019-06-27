60ddc3dab0740acfcde0d20d788bebd31bb6f5bd
alejogr170 <alejogr170@gmail.com>
2019-06-27 12:43:54 +0200

    solucionando_problema_fin

--- lista_de_la_compra.py
+++ lista_de_la_compra.py
@@ -8,7 +8,8 @@ input_usuario = ""
 
 while input_usuario != "FIN":
     input_usuario = input("Escribe lo que necesites (para salir escribe FIN) :")
-    lista_de_la_compra.append(input_usuario)
+    if input_usuario != "FIN":
+        lista_de_la_compra.append(input_usuario)
 
 cantidad_de_elementos = len(lista_de_la_compra)
 indice = 0

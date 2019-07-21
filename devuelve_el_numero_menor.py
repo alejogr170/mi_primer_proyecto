4c5679bab40bdd415c0e8787b38cb0965776f933
alejogr170 <alejogr170@gmail.com>
2019-07-21 15:49:43 +0200

    devuelve_el_menor

--- devuelve_el_numero_menor.py
+++ devuelve_el_numero_menor.py
@@ -8,4 +8,17 @@ while len(numeros_usuario) < 10:
     while not numero_a_introducir.isdigit():
         numero_a_introducir = input("Introduce un numero: ")
     print("Se ha introducido {}".format(numero_a_introducir))
+    numeros_usuario.append(int(numero_a_introducir))
     numero_a_introducir = ""
+
+numero_pequeño = numeros_usuario[0]
+
+for numero in numeros_usuario:
+    if numero_pequeño > numero:
+        numero_pequeño = numero
+
+print("Estos son los numeros que has introducido: {}".format(numeros_usuario))
+
+print()
+
+print("Y este es el numero mas pequeño: {}".format(numero_pequeño))
